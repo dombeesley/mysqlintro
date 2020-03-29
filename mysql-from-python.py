32e353962789bd3f54bce1b997ec61eb8eccd1f0
dombeesley <quigley_beesley@hotmail.com>
2020-03-29 07:29:21 +0000

    update using string interpolation

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -6,9 +6,10 @@ import pymysql
 connection = pymysql.connect(host='localhost',
                              db='Chinook')
 
-try: 
+try:
     with connection.cursor() as cursor:
-        cursor.execute("UPDATE Friends SET age = 22 WHERE name = 'bob';")
+        cursor.execute("UPDATE Friends SET age = %s WHERE name = %s;",
+                       (23, 'bob'))
         connection.commit()
 
 
